4d9a757d7ce407f58ab9a52e267796abf442f6f4
arvsyam <arivasyam31@gmail.com>
2022-04-23 14:52:07 +0700

    fix splash screen

--- app/src/main/java/com/adl/fortunecooking/MainActivity.kt
+++ app/src/main/java/com/adl/fortunecooking/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.adl.fortunecooking
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
         supportActionBar?.hide()
 
-        Handler().postDelayed({
-            val intent = Intent(this@MainActivity, HomeActivity::class.java)
-            startActivity(intent)
+//        val windowInsetsController =
+//            ViewCompat.getWindowInsetsController(window.decorView) ?: return
+//        // Configure the behavior of the hidden system bars
+//        windowInsetsController.systemBarsBehavior =
+//            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+//        // Hide both the status bar and the navigation bar
+//        windowInsetsController.hide(WindowInsetsCompat.Type.systemBars())
+
+
+        Handler().postDelayed({ //This method will be executed once the timer is over
+            // Start your app main activity
+            val i = Intent(this@MainActivity, HomeActivity::class.java)
+            startActivity(i)
+            // close this activity
+            finish()
         }, 3000)
+
+//        startActivity(Intent(this@MainActivity, HomeActivity::class.java))
+//        finish()
     }
 }
