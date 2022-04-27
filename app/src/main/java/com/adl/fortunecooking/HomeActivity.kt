a3375a327da3b2c695788f16d61be52a11067506
ymarios <yosefmariosupadman@gmail.com>
2022-04-28 00:31:25 +0700

    Fragment masih Error

--- app/src/main/java/com/adl/fortunecooking/HomeActivity.kt
+++ app/src/main/java/com/adl/fortunecooking/HomeActivity.kt
@@ -7,15 +7,47 @@ import android.view.View
 import android.view.Window
 import android.view.WindowInsets
 import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import com.adl.fortunecooking.fragments.AccountFragment
+import com.adl.fortunecooking.fragments.FavoritesFragment
+import com.adl.fortunecooking.fragments.HomeFragment
+import com.adl.fortunecooking.fragments.SearchFragment
+import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.card_holder.*
 
 class HomeActivity : AppCompatActivity() {
+    private val homeFragment = HomeFragment()
+    private val searchFragment = SearchFragment()
+    private val favoritesFragment = FavoritesFragment()
+    private val accountFragment = AccountFragment()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+        replacementFragment(homeFragment)
         setFullscreen()
 
+        bottom_navigation.setOnItemSelectedListener {
+            when(it.itemId) {
+                R.id.ic_home -> replacementFragment(homeFragment)
+                R.id.ic_search -> replacementFragment(searchFragment)
+                R.id.ic_favorite -> replacementFragment(favoritesFragment)
+                R.id.ic_account -> replacementFragment(accountFragment)
+            }
+            true
+        }
     }
+
+    private fun replacementFragment(fragment: Fragment){
+        if(fragment !=null){
+            val transaction = supportFragmentManager.beginTransaction()
+            transaction.replace(R.id.fragment_container, fragment)
+            transaction.commit()
+        }
+    }
+
+
+
 
     fun setFullscreen(){
         @Suppress("DEPRECATION")
