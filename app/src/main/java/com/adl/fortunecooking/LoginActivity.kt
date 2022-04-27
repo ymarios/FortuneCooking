80cb9469004351aa958eab05428a3206e3c561f2
RobbyRaffly <robbyaraffly12@gmail.com>
2022-04-27 22:42:01 +0700

    shared preferences

--- app/src/main/java/com/adl/fortunecooking/LoginActivity.kt
+++ app/src/main/java/com/adl/fortunecooking/LoginActivity.kt
@@ -3,6 +3,7 @@ package com.adl.fortunecooking
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CheckBox
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_login.*
@@ -11,14 +12,34 @@ class LoginActivity : AppCompatActivity() {
 
     lateinit var auth : FirebaseAuth
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         auth = FirebaseAuth.getInstance()
 
-        btnLogin.setOnClickListener({
+        val sharedPreference = getSharedPreferences("login", MODE_PRIVATE)
+        val username = sharedPreference.getString("username","")
+        val password = sharedPreference.getString("password","")
+        val Checked = sharedPreference.getBoolean("checbox", false)
+
+        txtEmail.setText(username)
+        txtPassword.setText(password)
+        checkBox.setChecked(Checked)
+
+        btnLogin.setOnClickListener{
+            var editor = sharedPreference.edit()
+            if(checkBox.isChecked){
+                editor.putString("username",txtEmail.text.toString())
+                editor.putString("password",txtPassword.text.toString())
+                editor.putBoolean("checkbox",true)
+                editor.commit()
+            }else{
+                editor.remove("username")
+                editor.commit()
+            }
             login()
-        })
+        }
 
         txtSignUp.setOnClickListener({
             val intent = Intent(this@LoginActivity,RegisterActivity::class.java)
@@ -36,4 +57,6 @@ class LoginActivity : AppCompatActivity() {
         }.addOnFailureListener{
                 exception -> Toast.makeText(applicationContext, exception.localizedMessage, Toast.LENGTH_LONG).show()}
     }
+
+
 }
