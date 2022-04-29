0ce524d1e8ebaf3b8b8a83c066802fe92a1fccd7
RobbyRaffly <robbyaraffly12@gmail.com>
2022-04-29 18:30:53 +0700

    layout akun blom fix

--- app/src/main/java/com/adl/fortunecooking/fragments/AccountFragment.kt
+++ app/src/main/java/com/adl/fortunecooking/fragments/AccountFragment.kt
@@ -1,11 +1,15 @@
 package com.adl.fortunecooking.fragments
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.adl.fortunecooking.LoginActivity
 import com.adl.fortunecooking.R
+import kotlinx.android.synthetic.main.fragment_account.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,13 +26,7 @@ class AccountFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,6 +35,26 @@ class AccountFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_account, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        btnLogOut.setOnClickListener({
+            activity?.let{
+                val intent = Intent (it, LoginActivity::class.java)
+                it.startActivity(intent)
+            }
+        })
+    }
+
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        arguments?.let {
+//            param1 = it.getString(ARG_PARAM1)
+//            param2 = it.getString(ARG_PARAM2)
+//        }
+//
+//    }
 
     companion object {
         /**
