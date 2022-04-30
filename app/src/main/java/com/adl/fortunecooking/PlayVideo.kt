3e47369e1ca41f3edf90255b2cf3002ee3e943bb
RobbyRaffly <robbyaraffly12@gmail.com>
2022-04-30 21:29:58 +0700

    layout play video

--- app/src/main/java/com/adl/fortunecooking/PlayVideo.kt
+++ app/src/main/java/com/adl/fortunecooking/PlayVideo.kt
@@ -2,6 +2,7 @@ package com.adl.fortunecooking
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.android.exoplayer2.source.MediaSource
@@ -15,29 +16,34 @@ import kotlinx.android.synthetic.main.activity_play_video.*
 class PlayVideo : AppCompatActivity() {
     val url:String = "https://firebasestorage.googleapis.com/v0/b/fortunecooking-1a02f.appspot.com/o/Resep_Cara_Membuat_Nugget_Ayam_Enak_How_to_Make_a_Delicious_Chicken_Nugget.mp4?alt=media&token=bdc5721b-2651-414f-b3b4-119e3a997b02"
     val url2:String= "rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mp4"
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_play_video)
-
-        var bandwithMeter : BandwidthMeter = DefaultBandwidthMeter()
-
-        var videoTrackSelectionFactory = AdaptiveTrackSelection.Factory()
-
-        var trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
-
-        var player =  SimpleExoPlayer.Builder(this)
-            .setTrackSelector(trackSelector)
-            .build()
-
-        videoPlayer.setPlayer(player)
-
-        val mediaSource: MediaSource = RtspMediaSource.Factory()
-            .createMediaSource(MediaItem.fromUri(url))
+        val myWebView: WebView = findViewById(R.id.webview)
+        myWebView.loadUrl(url)
 
 
-
-        player.setMediaSource(mediaSource)
-
-        player.prepare()
+//        var bandwithMeter : BandwidthMeter = DefaultBandwidthMeter()
+//
+//        var videoTrackSelectionFactory = AdaptiveTrackSelection.Factory()
+//
+//        var trackSelector = DefaultTrackSelector(videoTrackSelectionFactory)
+//
+//        var player =  SimpleExoPlayer.Builder(this)
+//            .setTrackSelector(trackSelector)
+//            .build()
+//
+//        videoPlayer.setPlayer(player)
+//
+//        val mediaSource: MediaSource = RtspMediaSource.Factory()
+//            .createMediaSource(MediaItem.fromUri(url))
+//
+//
+//
+//        player.setMediaSource(mediaSource)
+//
+//        player.prepare()
     }
 }
