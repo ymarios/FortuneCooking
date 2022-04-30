33e214da0d69e621477769f9d40669bfda33e32f
RobbyRaffly <robbyaraffly12@gmail.com>
2022-04-30 17:20:21 +0700

    layout play video

--- app/src/main/java/com/adl/fortunecooking/DetailResepActivity.kt
+++ app/src/main/java/com/adl/fortunecooking/DetailResepActivity.kt
@@ -1,11 +1,18 @@
 package com.adl.fortunecooking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_detail_resep.*
 
 class DetailResepActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_resep)
+
+        btnPlayVideo.setOnClickListener({
+            val intent = Intent(this@DetailResepActivity, PlayVideo::class.java)
+            startActivity(intent)
+        })
     }
 }
