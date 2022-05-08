7baf76650db231200494533e415085e618e201f7
arvsyam <arivasyam31@gmail.com>
2022-05-08 14:41:19 +0700

    insert Rating

--- app/src/main/java/com/adl/fortunecooking/DetailResepActivity.kt
+++ app/src/main/java/com/adl/fortunecooking/DetailResepActivity.kt
@@ -1,15 +1,25 @@
 package com.adl.fortunecooking
 
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+import com.adl.fortunecooking.fragments.RatingFragment
+
 import com.adl.fortunecooking.model.ResepModel
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.activity_detail_resep.*
+import kotlinx.android.synthetic.main.fragment_rating.*
 
 class DetailResepActivity : AppCompatActivity() {
     lateinit var data:ResepModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_resep)
@@ -29,8 +39,7 @@ class DetailResepActivity : AppCompatActivity() {
             txtDescriptionDetail.setText(data.Deskripsi)
             txtResepDetail.setText(data.Resep)
             txtStepDetail.setText(data.Step)
-            ratingDetail.setIsIndicator(true)
-            ratingDetail.setRating(data.rating.toFloat())
+
 
         }
         btnPlayVideo.setOnClickListener({
@@ -38,5 +47,38 @@ class DetailResepActivity : AppCompatActivity() {
             intent.putExtra("data",data)
             startActivity(intent)
         })
+
+        btn_set_rating.setOnClickListener({
+            showDialog()
+        })
+
+
     }
+    fun showDialog() {
+        val fragmentManager = supportFragmentManager
+        val newFragment = RatingFragment()
+            // The device is smaller, so show the fragment fullscreen
+            val transaction = fragmentManager.beginTransaction()
+            // For a little polish, specify a transition animation
+            transaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+            // To make it fullscreen, use the 'content' root view as the container
+            // for the fragment, which is always the root view for the activity
+            transaction
+                .add(android.R.id.content, newFragment)
+                .addToBackStack(null)
+                .commit()
+        Log.d("popup","pop")
+    }
+
+    override fun onCreateDialog(id: Int): Dialog {
+        return super.onCreateDialog(id)
+        val builder = AlertDialog.Builder(this)
+        builder.create()
+
+    }
+
+    fun insertRate(id:String){
+
+    }
+
 }
