deb97eae76c7a3d5b91f8932ffcbf84d5c67b993
ymarios <yosefmariosupadman@gmail.com>
2022-05-09 20:00:42 +0700

    fragment Account show data & Edit Profile Activity

--- app/src/main/java/com/adl/fortunecooking/fragments/AccountFragment.kt
+++ app/src/main/java/com/adl/fortunecooking/fragments/AccountFragment.kt
@@ -3,29 +3,39 @@ package com.adl.fortunecooking.fragments
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.adl.fortunecooking.DetailResepActivity
 import com.adl.fortunecooking.LoginActivity
+import com.adl.fortunecooking.ProfileEditActivity
 import com.adl.fortunecooking.R
+import com.bumptech.glide.Glide
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_account.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AccountFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+//// TODO: Rename parameter arguments, choose names that match
+//// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+//private const val ARG_PARAM1 = "param1"
+//private const val ARG_PARAM2 = "param2"
+//
+///**
+// * A simple [Fragment] subclass.
+// * Use the [AccountFragment.newInstance] factory method to
+// * create an instance of this fragment.
+// */
 class AccountFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    // TODO: Rename and change types of parameters
+//    private var param1: String? = null
+//    private var param2: String? = null
+
+    private lateinit var firebaseAuth: FirebaseAuth
 
 
 
@@ -40,6 +50,9 @@ class AccountFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+        loadUserInfo()
+
         btnLogOut.setOnClickListener({
             activity?.let{
                 val intent = Intent (it, LoginActivity::class.java)
@@ -48,9 +61,48 @@ class AccountFragment : Fragment() {
         })
 
         btnEditProfil.setOnClickListener({
-
+            activity?.let{
+                val intent = Intent(it, ProfileEditActivity::class.java)
+                it.startActivity(intent)
+                }
         })
     }
+
+    private fun loadUserInfo() {
+        //db referencee to load user info
+        val ref = FirebaseDatabase.getInstance().getReference("Users")
+        ref.child(firebaseAuth.uid!!)
+            .addValueEventListener(object : ValueEventListener{
+                override fun onDataChange(snapshot: DataSnapshot) {
+                    val email = "${snapshot.child("email").value}"
+                    val name = "${snapshot.child("name").value}"
+                    val profileImage = "${snapshot.child("profileImage").value}"
+                    val uid = "${snapshot.child("uid").value}"
+
+                    //set data
+                    txtNamaAcc.text = name
+                    txtEmailAcc.text = email
+
+                    //set image
+                    try {
+                        Glide.with(this@AccountFragment)
+                            .load(profileImage)
+                            .placeholder(R.drawable.ic_person_gray)
+                            .into(imgProfile)
+                    }
+                    catch (e: Exception){
+
+                    }
+
+
+                }
+
+                override fun onCancelled(error: DatabaseError) {
+
+                }
+            })
+    }
+
 
 //    override fun onCreate(savedInstanceState: Bundle?) {
 //        super.onCreate(savedInstanceState)
@@ -61,23 +113,23 @@ class AccountFragment : Fragment() {
 //
 //    }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AccountFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AccountFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+//    companion object {
+//        /**
+//         * Use this factory method to create a new instance of
+//         * this fragment using the provided parameters.
+//         *
+//         * @param param1 Parameter 1.
+//         * @param param2 Parameter 2.
+//         * @return A new instance of fragment AccountFragment.
+//         */
+//        // TODO: Rename and change types and number of parameters
+//        @JvmStatic
+//        fun newInstance(param1: String, param2: String) =
+//            AccountFragment().apply {
+//                arguments = Bundle().apply {
+//                    putString(ARG_PARAM1, param1)
+//                    putString(ARG_PARAM2, param2)
+//                }
+//            }
+//    }
 }
